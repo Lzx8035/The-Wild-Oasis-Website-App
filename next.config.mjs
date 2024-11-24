20406bdf5cd464ecfaa16423490b14a3103c30a6
lzx8035 <lzx8035@gmail.com>
2024-11-24 14:19:17 -0800

    Fetch and Display Cabins with Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pwrfjejoyxieuwfbpvni.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+  // output: "export",
+};
 
 export default nextConfig;
+
+// Path: "https://pwrfjejoyxieuwfbpvni.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+// 这是 Next.js 的一个安全特性，目的是：防止恶意图片URL注入;控制哪些外部图片源可以被使用;避免性能问题;
