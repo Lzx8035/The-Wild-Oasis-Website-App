b42036927668e2ca1e6de43d05673c11733b0178
lzx8035 <lzx8035@gmail.com>
2024-12-08 22:49:14 -0800

    Remove Reservation Immediately with useOptimistic

---
+++ app/account/_components/SignInButton.js
@@ -0,0 +1,22 @@
+// Since this Button is a server component, we can not use onClick fn
+// instead, we use Server Action
+
+import { signInAction } from "../_lib/actions";
+
+function SignInButton() {
+  return (
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
+  );
+}
+
+export default SignInButton;
